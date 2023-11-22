fb7fd68c116d9f47a99d9e8fd665a4555abb0951
joannaberlin <joanna.kot.ok@gmail.com>
2023-11-22 12:18:02 +0100

    Add kata solution - strings, arrays, fundamentals

---
+++ title-case/index.js
@@ -0,0 +1,66 @@
+/*
+https://www.codewars.com/kata/5202ef17a402dd033c000009/train/javascript
+
+A string is considered to be in title case if each word in the string is either (a)
+capitalised (that is, only the first letter of the word is in upper case) or (b)
+considered to be an exception and put entirely into lower case unless it is the first
+word, which is always capitalised.
+
+Write a function that will convert a string into title case, given an optional list of
+exceptions (minor words). The list of minor words will be given as a string with each
+word separated by a space. Your function should ignore the case of the minor words
+string -- it should behave in the same way even if the case of the minor word string is
+changed.
+
+Arguments (Haskell)
+First argument: space-delimited list of minor words that must always be lowercase except
+for the first word in the string.
+Second argument: the original string to be converted.
+Arguments (Other languages)
+First argument (required): the original string to be converted.
+Second argument (optional): space-delimited list of minor words that must always be
+lowercase except for the first word in the string. The JavaScript/CoffeeScript tests will
+pass undefined when this argument is unused.
+Example
+titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
+titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
+titleCase('the quick brown fox') // should return: 'The Quick Brown Fox
+*/
+
+function titleCase(title, minorWords) {
+	let finalArr = [];
+	const words = title.split(' ');
+	if (title === '') return '';
+	if (minorWords === undefined) {
+		return words
+			.map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+			.join(' ');
+	}
+	const wordsToCompare = minorWords
+		.split(' ')
+		.map((word) => word[0].toLowerCase() + word.slice(1).toLowerCase());
+	const resultArr = words.map((word) => word.toLowerCase());
+	for (let item of resultArr) {
+		for (let elem of wordsToCompare) {
+			if (item === elem) {
+				finalArr.push(item);
+			}
+		}
+	}
+	const arr = resultArr.map((word) =>
+		!finalArr.includes(word) ? word[0].toUpperCase() + word.slice(1) : word
+	);
+	const result = arr.join(' ');
+	return result[0].toUpperCase() + result.slice(1);
+}
+
+console.log(titleCase(''));
+//''
+console.log(titleCase('a clash of KINGS', 'a an the of'));
+//'A Clash of Kings'
+console.log(titleCase('THE WIND IN THE WILLOWS', 'The In'));
+//'The Wind in the Willows'
+console.log(titleCase('the quick brown fox'));
+//'The Quick Brown Fox'
+console.log(titleCase('First a of in', 'an often into'));
+//'First A Of In'
