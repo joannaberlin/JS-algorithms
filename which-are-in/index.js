375ba6ffee14acaf0e1d03c74a27926de1ce16db
joannaberlin <joanna.kot.ok@gmail.com>
2023-11-23 10:07:59 +0100

    Add solution to kata - loops, array methods

---
+++ which-are-in/index.js
@@ -0,0 +1,56 @@
+/*
+https://www.codewars.com/kata/550554fd08b86f84fe000a58/train/javascript
+
+Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order
+of the strings of a1 which are substrings of strings of a2.
+
+Example 1:
+a1 = ["arp", "live", "strong"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns ["arp", "live", "strong"]
+
+Example 2:
+a1 = ["tarp", "mice", "bull"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns []
+
+Notes:
+Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+Beware: In some languages r must be without duplicates.
+*/
+
+function inArray(array1, array2) {
+	let result = [];
+	for (let str of array1) {
+		for (let string of array2) {
+			if (string.includes(str) && !result.includes(str)) {
+				result.push(str);
+			}
+		}
+	}
+	return result.sort();
+}
+// solution without loops - filter, match, sort
+function inArray(array1, array2) {
+	return array1.filter((a1) => array2.find((a2) => a2.match(a1))).sort();
+}
+
+console.log(
+	inArray(
+		['live', 'strong', 'arp'],
+		['lively', 'alive', 'harp', 'sharp', 'armstrong']
+	)
+);
+//["arp", "live", "strong"]
+console.log(
+	inArray(
+		['tarp', 'mice', 'bull'],
+		['lively', 'alive', 'harp', 'sharp', 'armstrong']
+	)
+);
+//[]
