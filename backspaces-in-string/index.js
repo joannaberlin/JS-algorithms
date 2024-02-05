6c6e7a3bdfdd28833d5ca08beed0c54f178f8603
joannaberlin <joanna.kot.ok@gmail.com>
2024-02-05 10:38:15 +0100

    Add kata's solution

--- backspaces-in-string/index.js
+++ backspaces-in-string/index.js
@@ -1,13 +1,50 @@
+/*
+https://www.codewars.com/kata/5727bb0fe81185ae62000ae3/train/javascript
+
+Assume "#" is like a backspace in string. This means that string "a#bc#d" actually
+is "bd"
+
+Your task is to process a string with "#" symbols.
+
+Examples
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+*/
+
 function cleanString(s) {
-	if (s === '') return '';
-	//having a num of #, it should remove that num of chars before with all these #
-	//repeat on changed string
+	let result = [];
+	for (let char of s) {
+		if (char === '#' && result.length > 0) {
+			result.pop();
+		} else {
+			result.push(char);
+		}
+	}
+	if (result[0] === '#') {
+		return result.join('').slice(1);
+	}
+	return result.join('');
+}
+
+//It could be like that
+function cleanString(s) {
+	const result = [];
+	for (const c of s) {
+		if (c === '#') {
+			result.pop();
+		} else {
+			result.push(c);
+		}
+	}
+	return result.join('');
 }
 
 //Test cases
-cleanString('abc#d##c');
+console.log(cleanString('abc#d##c'));
 //'ac'
-cleanString('abc####d##c#');
+console.log(cleanString('abc####d##c#'));
 //''
-// cleanString('');
+console.log(cleanString(''));
 //''
