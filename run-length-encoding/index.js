a1918243fa24b80d710f5c138be829b43a23cf25
joannaberlin <joanna.kot.ok@gmail.com>
2024-02-20 08:59:26 +0100

    Add kata's solution

---
+++ run-length-encoding/index.js
@@ -0,0 +1,56 @@
+/*
+https://www.codewars.com/kata/546dba39fa8da224e8000467/train/javascript
+
+Run-length encoding (RLE) is a very simple form of data compression in which runs of data
+(that is, sequences in which the same data value occurs in many consecutive data elements) are
+stored as a single data value and count, rather than as the original run. Wikipedia
+
+Task
+Your task is to write such a run-length encoding. For a given string, return a list (or array) of pairs
+(or arrays) [ (i1, s1), (i2, s2), …, (in, sn) ], such that one can reconstruct the original string by
+replicating the character sx ix times and concatening all those strings. Your run-length encoding should
+be minimal, ie. for all i the values si and si+1 should differ.
+
+Examples
+As the article states, RLE is a very simple form of data compression. It's only suitable for runs of data,
+as one can see in the following example:
+
+runLengthEncoding("hello world!")
+ //=>      [[1,'h'], [1,'e'], [2,'l'], [1,'o'], [1,' '], [1,'w'], [1,'o'], [1,'r'], [1,'l'], [1,'d'], [1,'!']]
+It's very effective if the same data value occurs in many consecutive data elements:
+
+runLengthEncoding("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb")
+ // => [[34,'a'], [3,'b']]
+*/
+
+const runLengthEncoding = (str) => {
+	const arr = str.split('');
+	const items = arr
+		.map((item, i) => {
+			if (item !== arr[i + 1] && item !== ' ') {
+				return item + ' ';
+			}
+			return item;
+		})
+		.join('')
+		.split(' ')
+		.slice(0, -1)
+		.map((item) => (item === '' ? ' ' : item));
+
+	return items.map((item) => [item.length, item[0]]);
+};
+
+console.log(runLengthEncoding(''));
+//[]
+console.log(runLengthEncoding('abc'));
+//[[1,'a'],[1,'b'],[1,'c']]
+console.log(runLengthEncoding('hello world!'));
+//[[1,'h'],[1,'e'],[2,'l'],[1,'o'],[1,' '],[1,'w'],[1,'o'],[1,'r'],[1,'l'],[1,'d'],[1,'!']]
+console.log(
+	runLengthEncoding(
+		'WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW'
+	)
+);
+//[[12,'W'],[1,'B'],[12,'W'],[3,'B'],[24,'W'],[1,'B'],[14,'W']]
+console.log(runLengthEncoding('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb'));
+//[[34,'a'], [3,'b']]
